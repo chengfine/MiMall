1d2b4b6d8c1d4494be7e57bcd4c963e274b580a3
chenngLee <licheng736560926@outlook.com>
2020-04-20 10:50:08 +0800

    新建目录

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <router-view>
+
+    </router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-// import axios from 'axios'
-import jsonp from 'jsonp'
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  },
+
+},
   data() {
     return {
-      age: 30,
-      data: ''
+      
     }
   },
   mounted() {
-    let url = '/activity/servicetime'
-    // axios.get(url).then((e) => {
-    //   console.log(e)
-    // })
-    jsonp(url,(err, res) => {
-      let result = res;
-      this.data = result
-    })
     
   }
 }
