0d230bb517490e983221457336d32dc439ed89ed
chenngLee <licheng736560926@outlook.com>
2020-05-09 23:07:56 +0800

    产品站和吸顶

--- src/components/ProductParam.vue
+++ src/components/ProductParam.vue
@@ -1,13 +1,13 @@
 <template>
-    <div class="nav-bar">
+    <div class="nav-bar" :class="{'is_ficed':isFixed}">
         <div class="container">
             <div class="pro-title">
                 小米8
             </div>
             <div class="pro-param">
-                <a href="">概述</a><span>|</span>
-                <a href="">参数</a><span>|</span>
-                <a href="">用户评价</a><span>|</span>
+                <a href="javascript:;">概述</a><span>|</span>
+                <a href="javascript:;">参数</a><span>|</span>
+                <a href="javascript:;">用户评价</a>
                 <slot name="buy"></slot>
             </div>
         </div>
@@ -17,10 +17,57 @@
 <script>
     export default {
         name: 'nav-bar',
+        data() {
+            return {
+                isFixed : false
 
+            }
+        },
+        mounted(){
+            window.addEventListener('scroll', this.initHeight);
+        },
+        methods:{
+            initHeight(){
+                let scorllTop = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop;
+                this.isFixed = scorllTop > 152 ? true : false;
+            }
+        },
+        destroyed() {
+            window.removeEventListener('scroll', this.initHeight,false);
+        },
     }
 </script>
 
 <style lang="scss">
-
+@import './../assets/scss/config.scss';
+@import './../assets/scss/mixin.scss';
+    .nav-bar{
+        height: 70px;
+        line-height: 70px;
+        border-top: 1px solid $colorH;
+        background-color: $colorG;
+        box-shadow: 0 5px 5px $colorE;
+        &.is_ficed{
+            position: fixed;
+            top: 0;
+            width: 100%;
+        }
+        .container{
+            @include flex();
+            .pro-title{
+                font-size: $fontH;
+                color: $colorB;
+                font-weight: 400;
+            }
+            .pro-param{
+                font-size: $fontJ;
+                span{
+                    margin: 0 10px;
+                }
+                a{
+                    color: $colorC;
+                }
+            }
+        }
+    }
 </style>
--- src/pages/product.vue
+++ src/pages/product.vue
@@ -1,19 +1,43 @@
 <template>
-    <div>
-        <Product-param>
-            <template v-slot:buy>
-                <button class="btn">立即购买</button>
-            </template>
-        </Product-param>
-    </div>
+  <div class="product">
+    <Product-param>
+      <template v-slot:buy>
+        <button class="btn">立即购买</button>
+      </template>
+    </Product-param>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+    <div class="h">123</div>
+
+  </div>
 </template>
 <script>
-    import ProductParam from './../components/ProductParam'
-    export default{
-        name: 'product', // 组件名称或者页面名称
-        components: {
-            ProductParam
-        }
-        
-    }
+import ProductParam from "./../components/ProductParam";
+export default {
+  name: "product", // 组件名称或者页面名称
+  components: {
+    ProductParam
+  }
+};
 </script>
+<style lang="scss">
+.product {
+    div.h{
+        height: 200px;
+    }
+  button {
+    margin-left: 10px;
+  }
+}
+</style>
