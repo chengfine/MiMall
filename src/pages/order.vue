90fa12a8cf12d4eca03155ffc89688a59b6678a6
chenngLee <licheng736560926@outlook.com>
2020-05-17 23:16:07 +0800

    order.vue中添加serverbar

--- src/pages/order.vue
+++ src/pages/order.vue
@@ -2,17 +2,20 @@
     <div>
         <order-header></order-header>
         <Router-view></Router-view>
+        <server-bar></server-bar>
         <nav-footer></nav-footer>
     </div>
 </template>
 <script>
     import OrderHeader from './../components/OrderHeader'
+    import ServerBar from './../components/ServerBar'
     import NavFooter from './../components/NavFooter'
     export default{
         name: 'order', // 组件名称或者页面名称
         components: {
             OrderHeader,
-            NavFooter
+            NavFooter,
+            ServerBar
         }
     }
 </script>
